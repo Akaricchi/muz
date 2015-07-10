9fe5bbe58e1705cff55d848382dd8f7cc9fb012e
Andrew "Akari" Alexeyew <akari@dbc.1gb.ua>
2015-07-10 09:28:00 +0300

    fixed the llpractice importer

--- muz/beatmap/formats/tianyi9.py
+++ muz/beatmap/formats/tianyi9.py
@@ -19,8 +19,8 @@ extensions = ["json"]
 locations = ["beatmaps"]
 
 def read(fobj, filename, bare=False, options=None):
-    raw = fobj.read().decode('utf-8')
-    data = json.loads(raw[raw.index('{'):].decode('utf-8', errors='ignore'))
+    raw = fobj.read().decode('utf-8', errors='ignore')
+    data = json.loads(raw[raw.index('{'):])
     musfile = data["audiofile"]
 
     if not musfile.endswith('.mp3') and not musfile.endswith('.ogg'):
