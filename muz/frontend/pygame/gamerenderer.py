47d6a7a9fda9432a19752e997c2f2b25d71513b0
Andrew "Akari" Alexeyew <akari@dbc.1gb.ua>
2015-07-19 19:59:02 +0300

    corrected a == check that should be an "is" check

--- muz/frontend/pygame/gamerenderer.py
+++ muz/frontend/pygame/gamerenderer.py
@@ -320,7 +320,7 @@ class GameRenderer(object):
 
             grad = None
 
-            if band.heldNote == note:
+            if band.heldNote is note:
                 if self.holdGradients:
                     grad = self.highlightGradient
                 clr1 = colors["highlight"]
